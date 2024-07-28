35df141b587017ade2887e856c360a8c8bffb922
Vincent <duongvv.webmail@gmail.com>
2024-07-28 13:41:52 -0700

    more dom events practice

---
+++ dom-events/js-file.js
@@ -0,0 +1,10 @@
+
+// Method 2
+const btn_2 = document.querySelector("#btn_method_2");
+btn_2.onclick = () => alert("Hello World - method 2");
+
+//Method 3
+const btn_3 = document.querySelector("#btn_method_3");
+btn_3.addEventListener("click", () => {
+  alert("Hello World - Method 3");
+});
